596e5a12af1251e6fdf6fe995b39ea514002125b
Dyneteve <dyneteve@hentaios.com>
2023-01-03 10:17:45 +0200

    base: Apply the SafetyNet workaround to PlayStore aswell
    Play Store is used for the new Play Integrity API, extend the hack
    to it aswell
    
    Test: Device Integrity and Basic Integrity passes.
    
    idoybh: Rewrote to fit how we currently apply the workaround
    
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: Id607cdff0b902f285a6c1b769c0a4ee4202842b1

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -48,6 +48,7 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.yaap.PixelPropsUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -178,6 +179,9 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        if (PixelPropsUtils.getIsFinsky()) {
+            throw new UnsupportedOperationException("Blocking safetynet attestation for finsky");
+        }
         for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
             if (ste.getClassName().contains("DroidGuard")) {
                 throw new UnsupportedOperationException("Blocking safetynet attestation");
